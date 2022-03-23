a0858befd2051818428728139e1fece7a6ea14a1
Carolyn <cghudson@uwalumni.com>
2022-03-23 15:52:27 -0500

    add quiz instructions and high scores html

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+var startTime = 60
+
+//click start button
+    //timer starts
+    //user presented with question
+    //when question is answered
+        //user is presented with next question
+    //when question is answered incorrectly, time subtracted from clock
+//quiz over when time = 0 || all quesitons answered
+    //user able to save initials and score

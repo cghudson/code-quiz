95ae64e1adf8ae5622d6ae4b2adcc653bf1871f5
Carolyn <cghudson@uwalumni.com>
2022-03-23 17:50:52 -0500

    define variables, hide questions and score

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,16 @@
-var startTime = 60
+var startTime = 40
+var finalScoreEl = document.querySelector(".final-score")
+var quizQuestionsEl = document.querySelector(".quiz-questions")
+
+finalScoreEl.style.display = "none"
+quizQuestionsEl.style.display = "none"
+
 
 //click start button
     //timer starts
     //user presented with question
     //when question is answered
         //user is presented with next question
-    //when question is answered incorrectly, time subtracted from clock
+    //if question is answered incorrectly, time subtracted from clock
 //quiz over when time = 0 || all quesitons answered
     //user able to save initials and score

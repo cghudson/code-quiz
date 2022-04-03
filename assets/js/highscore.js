690f2d9d2acc2f0e881ed44e649ad129ef713e8b
Carolyn <cghudson@uwalumni.com>
2022-04-02 20:55:18 -0500

    add clear high score button

--- assets/js/highscore.js
+++ assets/js/highscore.js
@@ -1,19 +1,24 @@
 var highScoreListEl = document.querySelector(".highScoreList");
+var clearScoreEl = document.querySelector(".clear");
+
 
 function showScore() {
   var highScoreArr = localStorage.getItem("codequiz");
   highScoreArr = JSON.parse(highScoreArr) || [];
   let data = "";
   for (let i = 0; i < highScoreArr.length; i++) {
-    data += `
-    <tr>
+    data += `<tr>
     <th scope="row">${i + 1}</th>
     <td>${highScoreArr[i].user} </td>
     <td>${highScoreArr[i].score}</td>
-  </tr>`;
+    </tr>`;
   }
-
   highScoreListEl.innerHTML = data;
 }
+
+clearScoreEl.addEventListener("click", function () {
+  localStorage.clear();
+  location.reload()
+});
 
 showScore();
